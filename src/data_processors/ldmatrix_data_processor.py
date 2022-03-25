c6bddf099dcdc97d717bcf6080033274090be42a
zutari-martinjpage <martinjpage@outlook.com>
2022-03-25 20:52:07 +0000

    updated ldmatrix processor to preprocess known mtags, multiple matches added to new row

--- src/data_processors/ldmatrix_data_processor.py
+++ src/data_processors/ldmatrix_data_processor.py
@@ -19,13 +19,13 @@ class LDMatrixDataProcessor(LDDataProcessorPrototype):
         return list(novel_mtag), list(duplicated_mtag)
 
     def find_correlated_snps(self, chromosome, ld_matrix, mtag_variants, assoc_variants):
-        ld_matrix = self._filter_matrix(ld_matrix, mtag_variants, assoc_variants)
-        highest_correlation = ld_matrix.max(axis="columns")
+        novel_mtags = list(set(mtag_variants).difference(set(assoc_variants)))
+        known_mtags = list(set(mtag_variants).intersection(set(assoc_variants)))
 
         correlated_snps = []
-        for mtag_variant, highest_r in highest_correlation.items():
-            assoc_snps = self._find_highest_correlation(ld_matrix, mtag_variant, highest_r)
-            correlated_snps.append([mtag_variant, assoc_snps, highest_r, chromosome])
+        self._add_known_mtags(correlated_snps, known_mtags, chromosome)
+        ld_matrix = self._filter_matrix(ld_matrix, novel_mtags, assoc_variants)
+        self._add_novel_mtags(correlated_snps, ld_matrix, chromosome)
         return correlated_snps
 
     def _groupby_chromosome(self, df):
@@ -37,10 +37,28 @@ class LDMatrixDataProcessor(LDDataProcessorPrototype):
     def _convert_to_dict(self, merged_df):
         return merged_df.T.to_dict()
 
+    def _add_known_mtags(self, correlated_snps, known_mtags, chromosome):
+        if len(known_mtags) == 0:
+            return correlated_snps
+        for snp in known_mtags:
+            correlated_snps.append([snp, snp, 1.0, chromosome])
+
     def _filter_matrix(self, ld_matrix, mtag_variants, assoc_variants):
         row_filter = ld_matrix.index.isin(mtag_variants)
         col_filter = ld_matrix.columns.isin(assoc_variants)
         return ld_matrix.loc[row_filter, col_filter]
 
+    def _add_novel_mtags(self, correlated_snps, ld_matrix, chromosome):
+        highest_correlation = ld_matrix.max(axis="columns")
+
+        for mtag_variant, highest_r in highest_correlation.items():
+            assoc_snps = self._find_highest_correlation(ld_matrix, mtag_variant, highest_r)
+            self._add_matches(correlated_snps, assoc_snps, mtag_variant, highest_r, chromosome)
+        return correlated_snps
+
     def _find_highest_correlation(self, ld_matrix, variant, highest_r):
-        return ", ".join(list(ld_matrix.columns[ld_matrix.loc[variant, :].values == highest_r]))
+        return list(ld_matrix.columns[ld_matrix.loc[variant, :].values == highest_r])
+
+    def _add_matches(self, correlated_snps, assoc_snps, mtag_variant, highest_r, chromosome):
+        for assoc_snp in assoc_snps:
+            correlated_snps.append([mtag_variant, assoc_snp, highest_r, chromosome])
--- src/unit_tests/test_ldmatirx_processor.py
+++ src/unit_tests/test_ldmatirx_processor.py
@@ -3,5 +3,8 @@ from src.models.ldmatrix_processor import LDMatrixProcessor
 
 class TestLDMatrixProcessor:
 
+    def setup(self):
+        config = {''}
+
     def test_calculate_highest_ld(self):
         pass
